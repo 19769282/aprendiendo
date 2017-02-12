61bd299d48de417a8ec313bd3c9f8c4a2e848dfc
Luis Miguel Castro <luismiguelcastr@gmail.com>
2017-02-12 17:57:50 -0400

    escribi una nueva linea
    en el archivo hola mundo

--- hola mundo.cpp
+++ hola mundo.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-std::cout << "Hola mundo" << endl;
+std::cout << "Hola mundo vienvenido a githob" << endl;
+std::cout << "una nueva herramienta para desarrolladores" << endl;
 return 0;
 }
 
