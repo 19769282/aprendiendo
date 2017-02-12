700d1b3d620c5f2fc94432d5015bf91b458c5340
Luis Miguel Castro <luismiguelcastr@gmail.com>
2017-02-12 19:20:59 -0400

    la calculadora cientifica compila

---
+++ calculadora cientifica.cpp
@@ -0,0 +1,162 @@
+#include <iostream> 
+#include <math.h> 
+  
+using namespace std; 
+  
+int RESULTADO; 
+float RESULTADO2; 
+  
+int Numero1; 
+int Numero2; 
+float Numero3; 
+float Numero4; 
+  
+int Multiplic(int a, int b) 
+{ 
+    return (a) * (b); 
+} 
+  
+int Divis(int a, int b) 
+{ 
+    return (a) / (b); 
+} 
+  
+int Sum(int a, int b) 
+{ 
+    return (a) + (b); 
+} 
+  
+int Rest(int a, int b) 
+{ 
+    return (a) - (b); 
+} 
+  
+float RaizCu(float a) 
+{ 
+    return sqrt(a); 
+} 
+  
+float Potenc(float base, float exponente) 
+{ 
+    return pow(base, exponente); 
+} 
+  
+int main(){ 
+    
+    int a; 
+    char b; 
+    
+    cout << "By Penyair. n" << endl; 
+    cout << "Que operacion desea realizar? (1 - Sumar, 2 - Restar, 3 - Multiplicar, 4 - Dividir, 5 - Raiz Cuadrada, 6 - Potencia, 7 - Seno, 8 - Coseno, 9 - Tangente)" << endl;
+    cin >> a; 
+    
+    switch(a) 
+    { 
+             case 1: 
+             cout << "Ingrese el primer digito:" << endl; 
+             cin >> Numero1; 
+              
+             cout << "Ingrese el segundo digito:" << endl; 
+             cin >> Numero2; 
+             RESULTADO = Sum(Numero1, Numero2); 
+             cout << "Resultado: " << RESULTADO << endl; 
+             break; 
+              
+             case 2: 
+             cout << "Ingrese el minuendo:" << endl; 
+             cin >> Numero1; 
+              
+             cout << "Ingrese el sustraendo:" << endl; 
+             cin >> Numero2; 
+              
+             RESULTADO = Rest(Numero1, Numero2); 
+             cout << "Resultado: " << RESULTADO << endl; 
+             break; 
+              
+             case 3: 
+             cout << "Ingrese el primer factor:" << endl; 
+             cin >> Numero1; 
+              
+             cout << "Ingrese el segundo factor:" << endl; 
+             cin >> Numero2; 
+              
+             RESULTADO = Multiplic(Numero1, Numero2); 
+             cout << "Resultado: " << RESULTADO << endl; 
+             break; 
+              
+             case 4: 
+             cout << "Ingrese el dividendo:" << endl; 
+             cin >> Numero1; 
+              
+             cout << "Ingrese el divisor:" << endl; 
+             cin >> Numero2; 
+              
+             RESULTADO = Divis(Numero1, Numero2); 
+             cout << "Resultado: " << RESULTADO << endl; 
+             break; 
+              
+             case 5: 
+             cout << "Ingrese el numero para realizar la raiz:" << endl;
+             cin >> Numero3; 
+              
+             RESULTADO2 = RaizCu(Numero3); 
+             cout << "Resultado: " << RESULTADO2 << endl; 
+             break; 
+              
+             case 6: 
+             cout << "Ingrese el numero a potenciar:" << endl; 
+             cin >> Numero3; 
+              
+             cout << "Ingrese el exponente:" << endl; 
+             cin >> Numero4; 
+              
+             RESULTADO2 = Potenc(Numero3, Numero4); 
+             cout << "Resultado: " << RESULTADO2 << endl; 
+             break; 
+              
+             case 7: 
+             cout << "Ingrese el angulo de seno:" << endl; 
+             cin >> Numero3; 
+              
+             RESULTADO2 = sin(Numero3); 
+             cout << "Resultado: " << RESULTADO2 << endl; 
+             break; 
+              
+             case 8: 
+             cout << "Ingrese el angulo de Coseno:" << endl; 
+             cin >> Numero3; 
+              
+             RESULTADO2 = cos(Numero3); 
+             cout << "Resultado: " << RESULTADO2 << endl; 
+             break; 
+              
+             case 9: 
+             cout << "Ingrese el angulo de Tangente:" << endl; 
+             cin >> Numero3; 
+              
+             RESULTADO2 = tan(Numero3); 
+             cout << "Resultado:" << RESULTADO2 << endl; 
+             break; 
+  
+             default: 
+             cout << "Operacion incorrecta. Vuelva a intentarlo... n" << endl;
+             cout << "********************************************" << endl;
+             cout << "******************************************** n" << endl;
+             main(); 
+             } 
+              
+             cin.get(); 
+             cout << "Desea salir? (1 - Si, 2 - No)" << endl; 
+             cin >> b; 
+              
+             switch(b){ 
+                       case '1': 
+                            return 0; 
+                            break; 
+                       case '2': 
+                            cout << "-------------------------------" << endl; 
+                            cout << "------------------------------- n" << endl; 
+                            main(); 
+                            break; 
+                            } 
+             }
